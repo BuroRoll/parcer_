d08cb6a6bbe713298fdd21c0dcc76d411e5ec6e9
Danil <BuroRoll@gmail.com>
2024-03-01 14:24:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from fastapi import FastAPI, Request, UploadFile
 from fastapi.params import File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
 from tempfile import NamedTemporaryFile
 
 from main import parce_file
 
 app = FastAPI()
-
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 templates = Jinja2Templates(directory="templates")
 
